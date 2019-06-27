8de175294ec6aacdaa254ba6a7a4343250236b1a
vagrant <vagrant@localhost.localdomain>
2019-06-27 09:24:34 +0000

    Fix:日報機能、 create.blade.phpコメントアウト削除

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,20 +5,19 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'Report.store']) !!}
-        <!-- <input class="form-control" name="user_id" value="" type="hidden"> valueの値を後で訂正。userと紐づけ -->
         <div class="form-group form-size-small">
-      <input class="form-control" name="reporting_time" type="date"> <!--date -->
+      <input class="form-control" name="reporting_time" type="date"> 
       <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group">
-        <input class="form-control" placeholder="Title" name="title" type="text"> <!--title -->
+        <input class="form-control" placeholder="Title" name="title" type="text"> 
         <span class="help-block">{{  $errors->first('title') }}</span>
       </div>
       <div class="form-group">
-        <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10"></textarea> <!--text -->
+        <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10"></textarea> 
         <span class="help-block">{{  $errors->first('contents') }}<span>
       </div>
-      <button type="submit" class="btn btn-success pull-right">Add</button> <!--add -->
+      <button type="submit" class="btn btn-success pull-right">Add</button> 
     {!! Form::close() !!}
   </div>
 </div>
